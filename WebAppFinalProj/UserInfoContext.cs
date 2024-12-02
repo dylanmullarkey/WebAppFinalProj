6b9d80f9c277bb9d278c2ed1cb1110d9d61c90f5
Dylan Mullarkey <DylanMullarkey10@gmail.com>
2024-12-02 15:56:54 -0500

    Create db context for UserInfo

---
+++ WebAppFinalProj/UserInfoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppFinalProj.Models;
+
+namespace WebAppFinalProj
+{
+    public class UserInfoContext : DbContext
+    {
+        public UserInfoContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<UserInfo> Users { get; set; }
+    }
+}
